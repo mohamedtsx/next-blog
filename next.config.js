9d3392a1699217134a9527e912e71be29c03055d
Mohamed Khaled <ma0068664@gmail.com>
2023-05-25 07:53:43 +0300

    add styled components

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+      config.module.rules.push({
+        test: /\.(ts|tsx)$/,
+        use: {
+          loader: 'babel-loader'
+        }
+      })
+      return config
+    }
+    
 }
 
 module.exports = nextConfig
