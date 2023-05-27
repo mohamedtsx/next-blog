9eb49c70dacf6ebb3e8accbc987f1b89bb9b8ac9
Mohamed Khaled <ma0068664@gmail.com>
2023-05-27 12:17:49 +0300

    fix fs module not found

--- next.config.js
+++ next.config.js
@@ -1,13 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack(config, options) {
+  webpack(config, { nextRuntime }) {
       config.module.rules.push({
         test: /\.(ts|tsx)$/,
         use: {
           loader: 'babel-loader'
         }
-      })
+      });
+
+      if (typeof nextRuntime === "undefined") {
+        const { IgnorePlugin } = require("webpack");
+        const ignoreFs = new IgnorePlugin({ resourceRegExp: /fs/ });
+        config.plugins.push(ignoreFs);
+      };
+      
       return config
     }
     
